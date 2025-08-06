3fc9dc5800bb596dba469e725b61ecd38ac57360
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-08-06 04:00:52 +0300

    add tests for bad_request

--- backend/tests/test_basic.py
+++ backend/tests/test_basic.py
@@ -5,3 +5,11 @@ async def test_basic(service_client):
     response = await service_client.post('/classify-message', json={'text': 'Привет от бека!'})
     assert response.status == 200
     assert response.text == 'Привет от бека!'
+
+async def test_error_empty_request(service_client):
+    response = await service_client.post('/classify-message')
+    assert response.status == 400
+
+async def test_error_request_without_text(service_client):
+    response = await service_client.post('/classify-message',json={'name': 'Sus'})
+    assert response.status == 400
