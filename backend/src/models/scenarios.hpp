99175d0c639ff7e15ad9cebb6371b1c9a5e01909
Becur <privezenov96@mail.ru>
2025-08-09 16:05:39 +0300

    added enum type scenarios

---
+++ backend/src/models/scenarios.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+enum class TypeRequest{
+    ACCESS_ORDER,
+    CANCEL_ORDER,
+    VOICE_MESSAGE,
+    VOICE_WISH,
+    CALL_PASSENGER,
+    CREATE_ROUTE,
+    CHOOSE_ROUTE,
+    BUSINESS,
+    HOME,
+    FIND,
+    СHANGE_FARE,
+    COUNT_              // count elements in enum
+};
