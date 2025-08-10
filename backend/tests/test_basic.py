4537a2fe846b76ad45bc499b2cb85b0388bad2fc
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-08-10 13:50:22 +0300

    add basic tests to mock analytics service

--- backend/tests/test_basic.py
+++ backend/tests/test_basic.py
@@ -1,10 +1,29 @@
 # Start via `make test-debug` or `make test-release`
 
 
-async def test_basic(service_client):
-    response = await service_client.post('/classify-message', json={'text': 'Привет от бека!'})
+async def test_basic(service_client, mockserver):
+    calls = []
+
+    @mockserver.json_handler('/cluster')
+    def _mock_cluster(request):
+        calls.append(request)
+        assert request.json.get('text') == 'Принять заказ'
+        return {
+            "scenario": "ACCEPT_ORDER",
+            "address": "",
+            "address_number": None,
+            "fare": ""
+        }
+
+    response = await service_client.post('/classify-message', json={'text': 'Принять заказ'})
     assert response.status == 200
-    assert response.text == 'Привет от бека!'
+
+    got = response.json()
+    expected = {"scenario": "ACCEPT_ORDER", "address": "", "address_number": None, "fare": ""}
+    assert got == expected
+
+    # опциональная проверка, что внешний сервис был вызван ровно 1 раз
+    assert len(calls) == 1
 
 async def test_error_empty_request(service_client):
     response = await service_client.post('/classify-message')
