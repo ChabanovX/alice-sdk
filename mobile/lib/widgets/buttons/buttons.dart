dc9468c405880817d77e8557f20abb16f34a30cb
ChabanovX <firstchabanov@gmail.com>
2025-08-12 15:32:31 +0300

    Add new buttons

---
+++ mobile/lib/widgets/buttons/buttons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:voice_assistant/main.dart';
+import 'package:voice_assistant/theme.dart';
+import 'package:voice_assistant/widgets/action_buttons/slide_action_button_stateless.dart';
+
+part 'accept_button.dart';
+part 'go_online_button.dart';
+part 'go_with_orders_button.dart';
