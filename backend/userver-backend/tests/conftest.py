a9aeaf161aaaf7a5dad0871d535577c6d734c645
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-08-17 08:16:51 +0300

    fix: tests

--- backend/userver-backend/tests/conftest.py
+++ backend/userver-backend/tests/conftest.py
@@ -1,11 +1,35 @@
 import pytest
+import pathlib
 
+from testsuite.databases.pgsql import discover
 
 pytest_plugins = [
     'pytest_userver.plugins.core',
+    'pytest_userver.plugins.postgresql',
 ]
 
 USERVER_CONFIG_HOOKS = ['patch_external_service']
+
+@pytest.fixture(scope='session')
+def service_source_dir():
+    """Path to root directory service."""
+    return pathlib.Path(__file__).parent.parent
+
+@pytest.fixture(scope='session')
+def initial_data_path(service_source_dir):
+    """Path for find files with data"""
+    return [
+        service_source_dir / 'postgresql/data',
+    ]
+
+@pytest.fixture(scope='session')
+def pgsql_local(service_source_dir, pgsql_local_create):
+    """Create schemas databases for tests"""
+    databases = discover.find_schemas(
+        'pg_service_template',
+        [service_source_dir.joinpath('postgresql')],
+    )
+    return pgsql_local_create(list(databases.values()))
 
 
 @pytest.fixture(scope='session')
